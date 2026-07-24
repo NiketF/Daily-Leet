7700c09c220902b04b3849afb8668f6295c0e10d
Niket Fuladi <fuladiniket.work@gmail.com>
2026-07-24 19:56:55 +0530

    Time: 81 ms (99.25%), Space: 15.5 MB (28.16%) - LeetHub

---
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def characterReplacement(self, s, k):
+        """
+        :type s: str
+        :type k: int
+        :rtype: int
+        """
+        left=0
+        max_count=0
+        freq={}
+        for right in range(len(s)):
+            freq[s[right]]=freq.get(s[right],0)+1
+            max_count=max(max_count,freq[s[right]])
+
+            if right - left+1-max_count>k:
+                freq[s[left]]-=1
+                left+=1
+        return len(s)-left
+            
+
+
+
+        
